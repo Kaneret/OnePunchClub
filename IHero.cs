3a98275592eece17b65835017218dd6dbcc9b03b
Kaneret <vedun1474@gmail.com>
2018-03-13 23:48:52 +0500

    Костяк
    Характеристики и параметры, их интерфейсы, плюс классы главного героя и
    противников, их интерфейс.

---
+++ IHero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePunchClub
+{
+    public interface IHero
+    {
+        Endurance endurance { get; } //выносливость для работы
+        Health health { get; }
+        Mood mood { get; } //настроение
+        Satiety satiety { get; } //сытость      
+
+        ActiveSlots ativeSlots { get; }
+        Armor armor { get; }
+        Energy energy { get; }
+        EnergyRegen eReg { get; }
+        Hitpoints hp { get; } //очки здоровья в бою
+        Precision precision { get; } //точность в бою
+
+        Dexterity dexterity { get; } //ловкость
+        Power power { get; } //сила
+        Stamina stamina { get; } //выносливость для боя
+    }
+}
---
+++ OtherHero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePunchClub
+{
+    class OtherHero : IHero
+    {
+        public Endurance endurance { get; }
+        public Health health { get; }
+        public Mood mood { get; }
+        public Satiety satiety { get; }
+
+        public ActiveSlots ativeSlots { get; }
+        public Armor armor { get; }
+        public Energy energy { get; }
+        public EnergyRegen eReg { get; }
+        public Hitpoints hp { get; }
+        public Precision precision { get; }
+
+        public Dexterity dexterity { get; }
+        public Power power { get; }
+        public Stamina stamina { get; }
+    }
+}
