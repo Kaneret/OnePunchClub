7fde3d78d19a95b504b61ee74068fa18d8efac83
Kaneret <vedun1474@gmail.com>
2018-03-27 22:38:31 +0500

    Характеристики, параметры и пара умений

---
+++ Skills/CommonPunch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePunchClub
+{
+    class CommonPunch : ISkill
+    {
+        public string Name { get { return "Удар кулаком"; } }
+
+        public string Description
+        {
+            get
+            {
+                return "Описание";
+            }
+        }
+
+        public bool Attack { get { return true; } }
+        public bool Block { get { return false; } }
+        public bool Evade { get { return false; } }
+
+        /// <summary>
+        /// для использования персонажем
+        /// </summary>
+        /// <param name="hero"></param>
+        /// <param name="opponent"></param>
+        public void Execute(MainHero hero, FightBot opponent)
+        {
+
+        }
+
+        /// <summary>
+        /// для использования ботом
+        /// </summary>
+        /// <param name="opponent"></param>
+        /// <param name="hero"></param>
+        public void Execute(FightBot opponent, MainHero hero)
+        {
+
+        }
+    }
+}
---
+++ Skills/ISkill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePunchClub
+{
+    public interface ISkill
+    {
+        string Name { get; }
+        string Description { get; }
+        bool Attack { get; }
+        bool Block { get; }
+        bool Evade { get; }//уворот
+        void Execute(MainHero hero, FightBot opponent);
+        void Execute(FightBot opponent, MainHero hero);
+    }
+}
